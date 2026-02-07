a48e3d635374a87d42338dfdd607fc2126c1a8ef
Ethan Yiu <ethan.cy.yiu@gmail.com>
2026-02-07 13:11:13 -0500

    voice
    t status#

--- FoodieTrack/backend/main.py
+++ FoodieTrack/backend/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI, Depends
 from typing import Annotated
 
 # Import the Auth0 JWT dependencies & helpers
-from backend.autho import get_current_user_id
+from autho import get_current_user_id
+from routes.voice import router as voice_router
 
 app = FastAPI()
+app.include_router(voice_router)
 
 # Example dependency for requiring an authenticated user and extracting their user_id
 def require_user(
